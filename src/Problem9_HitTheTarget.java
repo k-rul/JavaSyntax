fb0c685674367d18335d341ace01cb65fda46817
momchil-iliev <momchil.s.iliev@gmail.com>
2016-03-21 15:09:11 +0200

    Problems from 6 to 9

---
+++ src/Problem9_HitTheTarget.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem9_HitTheTarget {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+
+        for (int i = 1; i <= 20; i++) {
+            for (int j = 1; j <= 20; j++) {
+                if (i + j == input) {
+                    System.out.println(i + "+" + j + "=" + input);
+                }
+                if (i - j == input) {
+                    System.out.println(i + "-" + j + "=" + input);
+                }
+            }
+        }
+    }
+}
