3ca9d2b6997651b2794d4fa9fe00471d54507f52
momchil-iliev <momchil.s.iliev@gmail.com>
2016-03-21 11:31:25 +0200

    Initial commit

---
+++ src/Problem1_RectangleArea.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by Momchil on 19-Mar-16.
+ */
+public class Problem1_RectangleArea {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println(a*b);
+    }
+}
